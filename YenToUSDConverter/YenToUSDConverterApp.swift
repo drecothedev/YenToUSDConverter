edd5000fe2d60b2f43646553e8af1375d88545f3
drecothedev <drecothedev@gmail.com>
2025-10-04 23:24:14 +0900

    Initial Commit

---
+++ YenToUSDConverter/YenToUSDConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  YenToUSDConverterApp.swift
+//  YenToUSDConverter
+//
+//  Created by Andre jones on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct YenToUSDConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YenToUSDConverterTests/YenToUSDConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  YenToUSDConverterTests.swift
+//  YenToUSDConverterTests
+//
+//  Created by Andre jones on 10/4/25.
+//
+
+import Testing
+@testable import YenToUSDConverter
+
+struct YenToUSDConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
